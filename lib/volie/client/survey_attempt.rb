62478802e899cf523a8c914c433b2be92c0b27e7
Andrew Stephenson <Andrew.Stephenson123@gmail.com>
2018-10-02 15:44:16 -0600

    Fixed the survey attempt class

--- lib/volie/client/survey_attempt.rb
+++ lib/volie/client/survey_attempt.rb
@@ -4,11 +4,11 @@ module Volie
   module Client
     # This class is responsible for communicating with the Volie
     # Servers about Survey Attempts
-    class SurveyAttempt
+    class SurveyAttempt < Resource
       VALID_KEYS = %i[attempt_key call_key campaign_customer_key campaign_key created_at customer_key department_key
                       disposition_key deleted_at employee_key notes organization_key survey_attempt_key
                       survey_template_key survey_key updated_at user_key voice_mail_recording_key].freeze
-      define_rest_action :survey_attempt, except: :create
+      define_rest_actions :survey_attempt, except: :create
     end
   end
 end
