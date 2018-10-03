637ca5b84023b9fcc3588174a7372f935d523fd1
Andrew Stephenson <Andrew.Stephenson123@gmail.com>
2018-10-02 19:05:05 -0600

    Added organization key to sale

--- lib/volie/client/sale.rb
+++ lib/volie/client/sale.rb
@@ -13,10 +13,10 @@ module Volie
                       customer_number deal_book_date deal_number deal_type delivery_odometer extended_warranty_flag
                       fand_i_manager_name fand_i_manager_number lease_cap_cost_reduction lease_odometer_allowance
                       lease_odometer_penalty lease_residual_amount lease_residual_percentage monthly_payment
-                      retail_down_payment sale_key sales_manager_name sales_manager_number salesman_one_name
-                      salesman_one_number salesman_two_name salesman_two_number term total_amount_financed
-                      total_odometer_allowed vehicle_make vehicle_model vehicle_model_year vehicle_trim
-                      vehicle_type vehicle_vin].freeze
+                      organization_key retail_down_payment sale_key sales_manager_name sales_manager_number
+                      salesman_one_name salesman_one_number salesman_two_name salesman_two_number term
+                      total_amount_financed total_odometer_allowed vehicle_make vehicle_model vehicle_model_year
+                      vehicle_trim vehicle_type vehicle_vin].freeze
 
       class << self
         def find_or_create_by(attributes)
