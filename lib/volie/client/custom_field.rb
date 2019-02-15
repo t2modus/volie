2078c362847c776408fc132e4f267c05fadcc1bf
Andy Kallhoff <andykallhoff@gmail.com>
2019-02-15 14:34:17 -0700

    oops comment

--- lib/volie/client/custom_field.rb
+++ lib/volie/client/custom_field.rb
@@ -7,19 +7,7 @@ module Volie
       VALID_KEYS = %i[account_key custom_field_key modelable_key modelable_type created_at data_type
                       is_active source key value]
 
-      # EXAMPLE: t2_lead_id on the sale
-      # custom_field_key: random-1234678-rand-139084
-      # modelable_type: 'Sale'
-      # modelable_key: random-1234678-rand-139084
-      # data_type: 'string'
-      # is_active: true
-      # key: 't2_lead_id'
-      # value: '1'
-      # account_key: random-1234678-rand-139084
-      # source: 'Api'
-      # created_at: 12345678
-
-      define_rest_actions :custom_fields
+      define_rest_actions :custom_field
     end
   end
 end
