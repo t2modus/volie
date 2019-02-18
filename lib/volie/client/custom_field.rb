321eb3793957836298bd179c5523ba81261c350c
Andy Kallhoff <andykallhoff@gmail.com>
2019-02-18 08:23:38 -0700

    revises keys to include all of them

--- lib/volie/client/custom_field.rb
+++ lib/volie/client/custom_field.rb
@@ -4,8 +4,10 @@ module Volie
   module Client
     # This class is responsible for adding custom fields to whatever resource we are wanting
     class CustomField < Resource
-      VALID_KEYS = %i[account_key custom_field_key modelable_key modelable_type created_at data_type
-                      is_active source key value]
+      VALID_KEYS = %i[custom_field_key account_key data_import_custom_field_key data_import_field_key
+                      modelable_key modelable_type created_at data_type is_active key source updated_at value]
+      # the fields required to create this are:
+      # modelable_type modelable_key data_type key value
 
       define_rest_actions :custom_field
     end
